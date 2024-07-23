cb76206850f6bfcc6e304be1e1b7e9bd4c390173
cvn1004 <cvn1004@naver.com>
2024-07-23 12:24:09 +0900

    [BE] Feat: 기본 게시판 및 댓글 기능 추가
    - 기본 게시판 crud 기능 추가
    - 댓글 crud 기능 추가
    
    Resolves:

--- backend/ndolphin/src/main/java/com/web/ndolphin/repository/BoardRepository.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/repository/BoardRepository.java
@@ -1,10 +1,14 @@
 package com.web.ndolphin.repository;
 
 import com.web.ndolphin.domain.Board;
+import com.web.ndolphin.domain.BoardType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+
+import java.util.List;
 
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Long> {
 
+    List<Board> findByBoardType(BoardType type);
 }
