22e056e71d0c3c659c2334bd4c61d615418ed919
차정혁 <ckwjdgur1234@gmail.com>
2024-07-23 14:36:58 +0900

    [BE] fix: Favorite query 수정

--- backend/ndolphin/src/main/java/com/web/ndolphin/repository/FavoriteRepository.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/repository/FavoriteRepository.java
@@ -3,6 +3,7 @@ package com.web.ndolphin.repository;
 import com.web.ndolphin.domain.Favorite;
 import com.web.ndolphin.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -10,6 +11,7 @@ import java.util.Optional;
 @Repository
 public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
 
+    @Query("SELECT f FROM Favorite f WHERE f.user.userId = :userId AND f.board.id = :boardId")
     Optional<Favorite> findByUserIdAndBoardId(Long userId, Long boardId);
 
 }
