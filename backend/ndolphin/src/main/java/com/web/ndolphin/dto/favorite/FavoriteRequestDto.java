f59894660ae2f052fd43b2e3c77e4b2cb2671616
차정혁 <ckwjdgur1234@gmail.com>
2024-07-23 12:24:09 +0900

    [BE] Feat: FavoriteRequestDto 생성

--- backend/ndolphin/src/main/java/com/web/ndolphin/dto/favorite/FavoriteRequestDto.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/dto/favorite/FavoriteRequestDto.java
@@ -1,2 +1,20 @@
-package com.web.ndolphin.dto.favorite;public class FavoriteRequestDto {
+package com.web.ndolphin.dto.favorite;
+
+import com.web.ndolphin.domain.Board;
+import com.web.ndolphin.domain.User;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+public class FavoriteRequestDto {
+
+    private final Long user_id;
+    private final Long board_id;
+
 }
--- backend/ndolphin/src/main/java/com/web/ndolphin/repository/FavoriteRepository.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/repository/FavoriteRepository.java
@@ -1,2 +1,10 @@
-package com.web.ndolphin.repository;public class favoriteRepository {
+package com.web.ndolphin.repository;
+
+import com.web.ndolphin.domain.Favorite;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface FavoriteRepository extends JpaRepository<Favorite, Long> {
+
 }
