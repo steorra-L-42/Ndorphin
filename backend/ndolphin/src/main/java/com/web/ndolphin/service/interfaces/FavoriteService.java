ce1a1064334616911c257377bf14b691c03a5764
차정혁 <ckwjdgur1234@gmail.com>
2024-07-23 12:32:55 +0900

    [BE] fix: FavoriteService Interface 수정
     - 정의하지 않은 메서드 시그니쳐 정의

--- backend/ndolphin/src/main/java/com/web/ndolphin/service/interfaces/FavoriteService.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/service/interfaces/FavoriteService.java
@@ -1,4 +1,11 @@
 package com.web.ndolphin.service.interfaces;
 
+import com.web.ndolphin.dto.favorite.FavoriteRequestDto;
+
 public interface FavoriteService {
+
+    void addFavorite(FavoriteRequestDto favoriteRequestDto);
+
 }
+
+
