e21391a4ba85d5df5e41d0f338e374fac69dfc33
차정혁 <ckwjdgur1234@gmail.com>
2024-07-24 17:26:57 +0900

    [BE] Feat: CommentController추가
     - 등록, 수정, 삭제 api추가

---
+++ backend/ndolphin/src/main/java/com/web/ndolphin/controller/CommentController.java
@@ -0,0 +1,48 @@
+package com.web.ndolphin.controller;
+
+import com.web.ndolphin.dto.ResponseDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/v1/boards/{boardId}/comments")
+@RequiredArgsConstructor
+public class CommentController {
+
+    private final CommentService commentService;
+
+    @PostMapping("/{commentId}")
+    public ResponseEntity<ResponseDto> addFavorite(
+        @RequestBody CommentRequestDto commentRequestDto) {
+
+        ResponseEntity<ResponseDto> response = commentService.addComment(commentRequestDto);
+
+        return response;
+    }
+
+    @PutMapping("/{commentId}")
+    public ResponseEntity<ResponseDto> addFavorite(
+        @RequestBody CommentRequestDto commentRequestDto) {
+
+        ResponseEntity<ResponseDto> response = commentService.updateComment(commentRequestDto);
+
+        return response;
+    }
+
+    @DeleteMapping("/{commentId}")
+    public ResponseEntity<ResponseDto> removeFavorite(
+        @PathVariable Long userId,
+        @PathVariable Long commentId) {
+
+        ResponseEntity<ResponseDto> response = commentService.removeComment(userId, commentId);
+
+        return response;
+    }
+}
