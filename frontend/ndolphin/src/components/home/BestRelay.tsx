726d1b4247327753e5728080f0274c927dd9dbf0
elumiryy <yerim5988jg@naver.com>
2024-07-24 17:45:09 +0900

    [FE] Design: 베스트 릴레이 슬라이드 이동 버튼 추가

--- frontend/ndolphin/src/components/home/BestRelay.tsx
+++ frontend/ndolphin/src/components/home/BestRelay.tsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import { IoIosArrowDropleft, IoIosArrowDropright } from "react-icons/io";
 import RankingFilter from "./RankingFilter";
 import RankingDate from "./RankingDate";
 
@@ -6,14 +7,22 @@ const BestRelay = () => {
   const [rankingType, setRankingType] = useState("일간");
 
   return (
-    <div className="px-28 py-14 bg-[#FFFDEF]">
-      <div className="h-20 flex flex-col justify-around">
-        <div className="flex items-end">
-          <span className="pr-5 text-2xl font-bold underline decoration-[#FFDE2F] decoration-4 underline-offset-8">Best 릴레이북</span>
-          <RankingFilter updateRankingType={setRankingType} />
+    <div className="px-28 py-14 bg-[#FFFDEF] flex justify-between items-center">
+      <div className="flex items-center">
+        <button>
+          <IoIosArrowDropleft className="text-3xl text-[#565656]" />
+        </button>
+        <div className="h-20 px-10 flex flex-col justify-between">
+          <div className="flex items-end">
+            <span className="pr-5 text-2xl font-bold underline decoration-[#FFDE2F] decoration-4 underline-offset-8">Best 릴레이북</span>
+            <RankingFilter updateRankingType={setRankingType} />
+          </div>
+          <RankingDate type={rankingType} />
         </div>
-        <RankingDate type={rankingType} />
       </div>
+      <button>
+        <IoIosArrowDropright className="text-3xl text-[#565656]" />
+      </button>
     </div>
   );
 };
