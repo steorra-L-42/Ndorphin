faa85edec16db5950960bcf46f697709d8910c0a
cvn1004 <cvn1004@naver.com>
2024-07-24 10:58:51 +0900

    [BE] Refactor: 코드 컨벤션 수정
    - 코드 컨벤션 수정
    
    Resoloves:

--- backend/ndolphin/src/main/java/com/web/ndolphin/controller/BoardController.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/controller/BoardController.java
@@ -1,21 +1,20 @@
 package com.web.ndolphin.controller;
 
-import com.web.ndolphin.domain.Board;
 import com.web.ndolphin.domain.BoardType;
-import com.web.ndolphin.domain.Comment;
-import com.web.ndolphin.domain.User;
 import com.web.ndolphin.dto.ResponseDto;
 import com.web.ndolphin.dto.board.request.BoardUpdateRequestDto;
-import com.web.ndolphin.dto.comment.CommentDto;
-import com.web.ndolphin.repository.UserRepository;
 import com.web.ndolphin.service.BoardService;
-import com.web.ndolphin.service.impl.BoardServiceImpl;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
@@ -27,32 +26,44 @@ public class BoardController {
     // C - 게시물 생성
     @PostMapping("/{userId}")
     public ResponseEntity createBoard(@PathVariable("userId") Long userId,
-                                      @RequestBody BoardUpdateRequestDto boardUpdateRequestDto) {
-        return boardService.createBoard(userId, boardUpdateRequestDto);
+        @RequestBody BoardUpdateRequestDto boardUpdateRequestDto) {
+
+        ResponseEntity<ResponseDto> response = boardService.createBoard(userId,
+            boardUpdateRequestDto);
+        return response;
     }
 
     // R - 게시물 목록 조회 (타입별로 조회)/api/v1/boards?type={boardType}
     @GetMapping()
     public ResponseEntity<ResponseDto> getBoardsByType(@RequestParam("type") BoardType boardType) {
-        return boardService.getBoardsByType(boardType);
+
+        ResponseEntity<ResponseDto> response = boardService.getBoardsByType(boardType);
+        return response;
     }
 
     // R - 게시물 상세 조회 /api/v1/boards/{boardId}
     @GetMapping("/{boardId}")
     public ResponseEntity<ResponseDto> getBoardById(@PathVariable Long boardId) {
-        return boardService.getBoardById(boardId);
+
+        ResponseEntity<ResponseDto> response = boardService.getBoardById(boardId);
+        return response;
     }
 
     // U - 게시물 수정
     @PutMapping("/{boardId}")
     public ResponseEntity<ResponseDto> updateBoard(@PathVariable("boardId") Long boardId,
-                                                   @RequestBody BoardUpdateRequestDto boardUpdateRequestDto) {
-        return boardService.updateBoard(boardId, boardUpdateRequestDto);
+        @RequestBody BoardUpdateRequestDto boardUpdateRequestDto) {
+
+        ResponseEntity<ResponseDto> resonse = boardService.updateBoard(boardId,
+            boardUpdateRequestDto);
+        return resonse;
     }
 
     // D - 게시물 삭제
     @DeleteMapping("/{boardId}")
-    public ResponseEntity<ResponseDto> deleteBoard(@PathVariable("boardId") Long boardId) {
-        return boardService.deleteBoard(boardId);
+    public ResponseEntity<ResponseDto> deeteBoard(@PathVariable("boardId") Long boardId) {
+
+        ResponseEntity<ResponseDto> resonse = boardService.deleteBoard(boardId);
+        return resonse;
     }
 }
