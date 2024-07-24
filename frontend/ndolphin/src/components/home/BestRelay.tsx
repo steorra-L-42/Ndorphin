f3d694813d37a62491b7efaad236098d3872b179
elumiryy <yerim5988jg@naver.com>
2024-07-24 17:50:47 +0900

    [FE] Design: 랭킹 기간 패딩 추가

--- frontend/ndolphin/src/components/home/BestRelay.tsx
+++ frontend/ndolphin/src/components/home/BestRelay.tsx
@@ -12,12 +12,14 @@ const BestRelay = () => {
         <button>
           <IoIosArrowDropleft className="text-3xl text-[#565656]" />
         </button>
-        <div className="h-20 px-10 flex flex-col justify-between">
+        <div className="px-10 flex flex-col justify-between">
           <div className="flex items-end">
             <span className="pr-5 text-2xl font-bold underline decoration-[#FFDE2F] decoration-4 underline-offset-8">Best 릴레이북</span>
             <RankingFilter updateRankingType={setRankingType} />
           </div>
-          <RankingDate type={rankingType} />
+          <div className="py-4">
+            <RankingDate type={rankingType} />
+          </div>
         </div>
       </div>
       <button>
