f4fee92c7376744e85ceb01005c1fd1dce6821e1
박예진 <eksk9044@naver.com>
2024-07-24 16:58:07 +0900

    [FE] Design: 페이지네이션 컴포넌트 추가 - 페이지네이션 추가 및 디자인 - 릴레이북 목록 페이지에 페이지네이션 추가

--- frontend/ndolphin/src/components/relay/Book.tsx
+++ frontend/ndolphin/src/components/relay/Book.tsx
@@ -5,7 +5,7 @@ function Book() {
         <p className="mx-1 px-3 py-1 rounded-t-lg text-xs font-bold text-zinc-900 bg-amber-300">참여</p>
       </div>
       <img src="/assets/heart.png" className="w-9 absolute z-10 mt-3 ml-40" alt="#" />
-      <img src="/assets/표지.jpg" className="w-52 h-72 rounded-md relative z-0"></img>
+      <img src="/assets/cover.jpg" className="w-52 h-72 rounded-md relative z-0"></img>
       <p className="py-2 font-bold text-lg">혼자서 본 영화</p>
       <button className="w-32 px-2 py-1 flex justify-between items-center rounded-3xl border-2 border-solid border-zinc-300 font-bold text-zinc-800">
         <img src="/assets/aiSummaryButton.png" className="w-5" alt="#"></img>
