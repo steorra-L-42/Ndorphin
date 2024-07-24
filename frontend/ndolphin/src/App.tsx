a630462388227137b716bead132b1ad2ebf63815
elumiryy <yerim5988jg@naver.com>
2024-07-24 13:04:31 +0900

    [FE] Feat: react-router-dom 추가
    - 페이지 라우팅을 위해 react-router-dom을 추가함
    
    Resolves:
    Related to:

--- frontend/ndolphin/src/App.tsx
+++ frontend/ndolphin/src/App.tsx
@@ -1,10 +1,15 @@
 import React from "react";
 import "./App.css";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <BrowserRouter>
+        <Routes>
+          {/* <Route path="/example" element={<Example />}></Route> */}
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
