e884995f0b7a61547e75e1b1c34f0f854d50784a
차정혁 <ckwjdgur1234@gmail.com>
2024-07-25 13:20:34 +0900

    [BE] Fix: update시 데이터 넘겨주지 않음
     - update후 update된 데이터를 넘겨주지 않음
     - Project에 따라 이후 활성화

--- backend/ndolphin/src/main/java/com/web/ndolphin/service/impl/CommentServiceImpl.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/service/impl/CommentServiceImpl.java
@@ -1,13 +1,10 @@
 package com.web.ndolphin.service.impl;
 
-import com.web.ndolphin.common.ResponseCode;
-import com.web.ndolphin.common.ResponseMessage;
 import com.web.ndolphin.domain.Board;
 import com.web.ndolphin.domain.Comment;
 import com.web.ndolphin.domain.User;
 import com.web.ndolphin.dto.ResponseDto;
 import com.web.ndolphin.dto.comment.CommentRequestDto;
-import com.web.ndolphin.dto.comment.CommentResponseDto;
 import com.web.ndolphin.mapper.CommentMapper;
 import com.web.ndolphin.repository.BoardRepository;
 import com.web.ndolphin.repository.CommentRepository;
@@ -15,7 +12,6 @@ import com.web.ndolphin.repository.UserRepository;
 import com.web.ndolphin.service.interfaces.CommentService;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
@@ -55,22 +51,24 @@ public class CommentServiceImpl implements CommentService {
         try {
             Comment comment = commentRepository.findById(commentId)
                 .orElseThrow(() -> new IllegalArgumentException("Invalid comment ID"));
-            String nickName = userRepository.findNickNameById(comment.getUser().getUserId());
-            Long loveCnt = commentRepository.countLovesByCommentId(commentId);
-            String url = FileInfoRepository.getFile();
+//            String nickName = userRepository.findNickNameById(comment.getUser().getUserId());
+//            Long loveCnt = commentRepository.countLovesByCommentId(commentId);
+//            String url = FileInfoRepository.getFile();
+//            String url = null;
 
             comment.setContent(commentRequestDto.getContent());
 
-            CommentResponseDto commentResponseDto = CommentMapper.toDto(comment, nickName,
-                loveCnt, url);
+//            CommentResponseDto commentResponseDto = CommentMapper.toDto(comment, nickName,
+//                loveCnt, url);
+//
+//            ResponseDto<CommentResponseDto> responseBody = new ResponseDto<>(
+//                ResponseCode.SUCCESS,
+//                ResponseMessage.SUCCESS,
+//                commentResponseDto
+//            );
 
-            ResponseDto<CommentResponseDto> responseBody = new ResponseDto<>(
-                ResponseCode.SUCCESS,
-                ResponseMessage.SUCCESS,
-                commentResponseDto
-            );
-
-            return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+            return ResponseDto.success();
+//            return ResponseEntity.status(HttpStatus.OK).body(responseBody);
         } catch (Exception e) {
             return ResponseDto.databaseError(e.getMessage()); // 예외 발생 시 데이터베이스 에러 응답
         }
