c28834ddb1000e11f7522fe178edf6623b8c8a13
이성현 <vkse1934@naver.com>
2024-07-26 14:59:54 +0900

    [BE] Fix: ddl-auto update 에러 수정
    - ddl-auto update 시 data.sql 자동 실행 시 에러 발생-> 쿼리문 수정으로 해결

--- backend/ndolphin/src/main/resources/data.sql
+++ backend/ndolphin/src/main/resources/data.sql
@@ -1,5 +1,15 @@
 -- PointRule 데이터 삽입
 INSERT INTO point_rule (point_rule_id, reason, point, created_at)
-VALUES (1, 'POST', 10, CURRENT_TIMESTAMP),
-       (2, 'REACTION', 5, CURRENT_TIMESTAMP),
-       (3, 'LIKE', 2, CURRENT_TIMESTAMP);
+SELECT 1, 'POST', 10, CURRENT_TIMESTAMP
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM point_rule WHERE point_rule_id = 1);
+
+INSERT INTO point_rule (point_rule_id, reason, point, created_at)
+SELECT 2, 'REACTION', 5, CURRENT_TIMESTAMP
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM point_rule WHERE point_rule_id = 2);
+
+INSERT INTO point_rule (point_rule_id, reason, point, created_at)
+SELECT 3, 'LIKE', 2, CURRENT_TIMESTAMP
+FROM DUAL
+WHERE NOT EXISTS (SELECT 1 FROM point_rule WHERE point_rule_id = 3);
