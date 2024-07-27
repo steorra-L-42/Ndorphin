491e6156c88907d3d5421010307cafd9de382fa5
elumiryy <yerim5988jg@naver.com>
2024-07-27 21:36:06 +0900

    [FE] Design: 베스트 릴레이 부드러운 전환 효과 적용

--- frontend/ndolphin/src/components/home/MainRelayBook.tsx
+++ frontend/ndolphin/src/components/home/MainRelayBook.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import "../../css/home/MainRelayBook.css";
 
 interface Props {
   mainIndex: number;
@@ -58,34 +60,36 @@ const MainRelayBook = (props: Props) => {
     },
   ];
 
-  console.log(props.mainIndex);
-
   return (
-    <div className="grid grid-cols-2 ease-in">
-      <img className="w-[90%] rounded-xl shadow-[5px_5px_5px_5px_rgba(150,150,150,0.3)]" src={bookList[props.mainIndex].imgUrl} alt="" />
-      <div className="grid grid-rows-[auto_auto_auto_auto_auto]">
-        <div className="flex items-end">
-          <p className="text-6xl font-bold">{bookList[props.mainIndex].id}</p>
-          <p className="text-xl font-semibold">코딱지 대장 버티 1</p>
-        </div>
-        <div className="flex items-center">
-          <img className="w-10 h-10 rounded-[50%]" src="assets/profile1.png" alt="" />
-          <p className="pl-4 font-semibold">상상의 나무꾼</p>
-        </div>
-        <p className="text-justify">편의점에 근무하는 사람들이 모두 수상한 점을 하나씩 가지고 있는 내용이다. 뭐가 그렇게 수상한걸까..</p>
-        <hr className="h-[2px] bg-[#9E9E9E]" />
-        <div>
-          <div className="flex">
-            <p className="font-semibold text-[#333333]">조회수</p>
-            <p className="pl-2 text-[#565656]">216 회</p>
+    <TransitionGroup>
+      <CSSTransition key={props.mainIndex} timeout={300} classNames="fade">
+        <div className="w-[40%] absolute grid grid-cols-2">
+          <img className="w-[90%] rounded-xl shadow-[5px_5px_5px_5px_rgba(150,150,150,0.3)]" src={bookList[props.mainIndex].imgUrl} alt="" />
+          <div className="grid grid-rows-[auto_auto_auto_auto_auto]">
+            <div className="flex items-end">
+              <p className="text-6xl font-bold">{bookList[props.mainIndex].id}</p>
+              <p className="text-xl font-semibold">{bookList[props.mainIndex].title}</p>
+            </div>
+            <div className="flex items-center">
+              <img className="w-10 h-10 rounded-[50%]" src="assets/profile1.png" alt="" />
+              <p className="pl-4 font-semibold">상상의 나무꾼</p>
+            </div>
+            <p className="text-justify">편의점에 근무하는 사람들이 모두 수상한 점을 하나씩 가지고 있는 내용이다. 뭐가 그렇게 수상한걸까..</p>
+            <hr className="h-[2px] bg-[#9E9E9E]" />
+            <div>
+              <div className="flex">
+                <p className="font-semibold text-[#333333]">조회수</p>
+                <p className="pl-2 text-[#565656]">216 회</p>
+              </div>
+              <div className="flex">
+                <p className="font-semibold text-[#333333]">공감수</p>
+                <p className="pl-2 text-[#565656]">64 개</p>
+              </div>
+            </div>
           </div>
-          <div className="flex">
-            <p className="font-semibold text-[#333333]">공감수</p>
-            <p className="pl-2 text-[#565656]">64 개</p>
-          </div>
         </div>
-      </div>
-    </div>
+      </CSSTransition>
+    </TransitionGroup>
   );
 };
 
