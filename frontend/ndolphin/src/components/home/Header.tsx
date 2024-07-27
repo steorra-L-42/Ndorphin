bf8c28ae483021343525b774e9bff94cab917e1c
elumiryy <yerim5988jg@naver.com>
2024-07-27 17:22:23 +0900

    [FE] Fix: 특정 페이지에서 로고가 안보이는 오류 수정

--- frontend/ndolphin/src/components/home/Header.tsx
+++ frontend/ndolphin/src/components/home/Header.tsx
@@ -27,7 +27,7 @@ const Header = () => {
   return (
     <div className="w-full h-20 px-48 relative z-1 flex justify-between items-center shadow-[0_2px_5px_0_rgba(0,0,0,0.2)]">
       <div className="w-1/2 flex justify-between">
-        <img className="w-48 h-12" src="assets/logo.PNG" alt="" />
+        <img className="w-48 h-12" src="/assets/logo.PNG" alt="" />
         <div className="w-2/3 flex justify-around items-center text-[#6C6C6C] font-semibold">
           {menuList.map((menu) => (
             <p className="hover:pb-3 hover:text-black hover:underline decoration-[#FFDE2F] decoration-4 underline-offset-8 duration-300" key={menu.id}>
