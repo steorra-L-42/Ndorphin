30505388d93fef0b9879ae9779de174666faf94c
elumiryy <yerim5988jg@naver.com>
2024-07-27 15:47:00 +0900

    [FE] Design: 기본 양쪽 여백 48px로 변경

--- frontend/ndolphin/src/components/home/BestRelay.tsx
+++ frontend/ndolphin/src/components/home/BestRelay.tsx
@@ -9,7 +9,7 @@ const BestRelay = () => {
   const [rankingType, setRankingType] = useState("일간");
 
   return (
-    <div className="px-28 py-14 bg-[#FFFDEF]">
+    <div className="px-48 py-14 bg-[#FFFDEF]">
       <div className="flex items-end">
         <span className="pr-5 text-2xl font-bold underline decoration-[#FFDE2F] decoration-4 underline-offset-8">Best 릴레이북</span>
         <RankingFilter updateRankingType={setRankingType} />
