f29d065c89ecd859991d5b055e21d9029c4901da
elumiryy <yerim5988jg@naver.com>
2024-07-29 01:32:27 +0900

    [FE] Feat: 만약에 리스트 라우팅 경로 추가

--- frontend/ndolphin/src/App.tsx
+++ frontend/ndolphin/src/App.tsx
@@ -2,10 +2,11 @@
 import "./App.css";
 import { BrowserRouter, Route, Routes, useParams } from "react-router-dom";
 import Header from "./components/home/Header";
+import Home from "./pages/Home";
 import RelayBookList from "./pages/relay/RelayBookList";
 import RelayBookStart from "./pages/relay/RelayBookStart";
 import RelayBookDetail from "./pages/relay/RelayBookDetail";
-import Home from "./pages/Home";
+import IfList from "./pages/if/IfList";
 
 function App() {
   return (
@@ -13,10 +14,11 @@ function App() {
       <BrowserRouter>
         <Header />
         <Routes>
+          <Route path="/" element={<Home />}></Route>
           <Route path="/relaybooklist" element={<RelayBookList />}></Route>
           <Route path="/relaybookstart" element={<RelayBookStart />}></Route>
           <Route path="/relaybookdetail/:id" element={<RelayBookDetail />}></Route>
-          <Route path="/" element={<Home />}></Route>
+          <Route path="/iflist" element={<IfList />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
