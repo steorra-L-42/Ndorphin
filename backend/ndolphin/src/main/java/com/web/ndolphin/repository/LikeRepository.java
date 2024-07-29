daa5aaff524c0e5cb5c84ff90b8d06c386ebe8ae
차정혁 <ckwjdgur1234@gmail.com>
2024-07-29 15:30:10 +0900

    [BE] Feat: LikeRepository 추가
     - 좋아요가 저장될 repository

---
+++ backend/ndolphin/src/main/java/com/web/ndolphin/repository/LikeRepository.java
@@ -0,0 +1,10 @@
+package com.web.ndolphin.repository;
+
+import com.web.ndolphin.domain.Like;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface LikeRepository extends JpaRepository<Like, Long> {
+
+}
