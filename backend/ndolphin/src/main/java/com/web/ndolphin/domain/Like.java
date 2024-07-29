8a34d22d67573ca1438c45e4419451ee0fc0df23
차정혁 <ckwjdgur1234@gmail.com>
2024-07-29 15:30:52 +0900

    [BE] Refactor: 좋아요 entity 수정
     - love -> like로 수정

--- backend/ndolphin/src/main/java/com/web/ndolphin/domain/Like.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/domain/Like.java
@@ -1,19 +1,25 @@
 package com.web.ndolphin.domain;
 
-import jakarta.persistence.*;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import java.time.LocalDateTime;
 import lombok.Getter;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Entity
 @Getter
 @Setter
-public class Love {
+public class Like {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "love_id")
+    @Column(name = "like_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
