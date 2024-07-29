096bf488071efba4c31133b9608b09cc579cb1da
elumiryy <yerim5988jg@naver.com>
2024-07-29 21:30:36 +0900

    [FE] Design: 만약에 (의견) 수정, 삭제 버튼 디자인 변경

--- frontend/ndolphin/src/components/common/MeatballsMenu.tsx
+++ frontend/ndolphin/src/components/common/MeatballsMenu.tsx
@@ -3,19 +3,16 @@ import { HiOutlineDotsHorizontal } from "react-icons/hi";
 
 const MeatballsMenu = () => {
   return (
-    <ul className="menu menu-horizontal p-0 bg-[#E8E9EA] rounded-lg">
+    <ul className="menu menu-horizontal bg-base-200 rounded-box px-2 py-1">
       <li>
-        <details>
-          <summary className="px-4 py-1">설정</summary>
-          <ul className="p-2 bg-base-100 rounded-t-none relative z-10 ">
-            <li>
-              <a>수정</a>
-            </li>
-            <li>
-              <a>삭제</a>
-            </li>
-          </ul>
-        </details>
+        <a className="px-2 py-1">
+          <img src="/assets/updateIcon.png" alt="" />
+        </a>
+      </li>
+      <li>
+        <a className="px-2 py-1">
+          <img src="/assets/deleteIcon.png" alt="" />
+        </a>
       </li>
     </ul>
   );
