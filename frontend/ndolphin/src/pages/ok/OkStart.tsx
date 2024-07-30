dc21ea02d8bb3eb59e43bedf0343eafc5eba1d68
elumiryy <yerim5988jg@naver.com>
2024-07-31 03:06:58 +0900

    [FE] Design: 괜찮아 작성 모달 디자인
    - textarea 세로 길이 자동 증가 기능
    - 사진 4개 첨부 기능
    - 사진 한 번에 여러 개 선택 기능
    - 사진 4개 이상 안됨 기능
    - 이미지 2개씩 슬라이드 기능

--- frontend/ndolphin/src/pages/ok/OkStart.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const OkStart = () => {
-  return <div></div>;
-};
-
-export default OkStart;
---
+++ frontend/ndolphin/src/pages/ok/OkStartModal.tsx
@@ -0,0 +1,104 @@
+import React, { ChangeEvent, useEffect, useState } from "react";
+import { SlArrowLeft, SlArrowRight } from "react-icons/sl";
+
+const OkStartModal = () => {
+  const [imageList, setImageList] = useState<string[]>([]);
+  const [rowCount, setRowCount] = useState(1);
+  const [slideState, setSlideState] = useState(0); // 0 : 앞, 1 : 뒤
+  const [currentSlideList, setCurrentSlideList] = useState<string[]>([]);
+
+  const handleImageChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const files = event.target.files;
+    if (files) {
+      const newImageList: string[] = [];
+      Array.from(files).forEach((file) => {
+        const reader = new FileReader();
+        reader.onloadend = () => {
+          const result = reader.result as string;
+          newImageList.push(result);
+          if (newImageList.length === files.length) {
+            setImageList((prev) => {
+              const updatedList = [...prev, ...newImageList].slice(0, 4);
+              setCurrentSlideList(updatedList.slice(0, 2));
+              return updatedList;
+            });
+          }
+        };
+        reader.readAsDataURL(file);
+      });
+    }
+  };
+
+  const handleTextareaChange = (event: ChangeEvent<HTMLTextAreaElement>) => {
+    const rows = event.target.value.split(/\r\n|\r|\n/).length;
+    setRowCount(rows);
+  };
+
+  const handlePrev = () => {
+    setSlideState(0);
+    setCurrentSlideList(imageList.slice(0, 2));
+  };
+
+  const handleNext = () => {
+    setSlideState(1);
+    setCurrentSlideList(imageList.slice(2, 4));
+  };
+
+  useEffect(() => {
+    const targetTextarea = document.querySelector(`#target`) as HTMLTextAreaElement | null;
+    if (targetTextarea) {
+      targetTextarea.style.height = rowCount * 28 + "px";
+    }
+  }, [rowCount]);
+
+  return (
+    <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
+      <div className="w-[40%] max-h-[90%] p-5 bg-white rounded-2xl grid gap-3">
+        <div className="grid grid-cols-[1fr_8fr]">
+          <img className="w-11 h-11 rounded-[50%]" src="/assets/profile/profile3.png" alt="" />
+          <div className="max-h-[450px] grid gap-3 overflow-y-auto">
+            <textarea className="w-full min-h-28 text-xl font-medium outline-none resize-none" placeholder="당신의 고민은?" name="" id="target" onChange={(e) => handleTextareaChange(e)}></textarea>
+
+            <div className="grid grid-cols-2 gap-2 relative">
+              {imageList.length <= 2 || slideState === 0 ? (
+                <></>
+              ) : (
+                <button className="absolute top-1/2 left-2 translate-y-[-50%] z-50" onClick={handlePrev}>
+                  <SlArrowLeft className="text-4xl p-2 bg-white rounded-[50%] opacity-85" />
+                </button>
+              )}
+              {currentSlideList.map((image, index) => (
+                <img key={index} className="w-full h-72 rounded-md object-cover" src={image} alt="" />
+              ))}
+              {imageList.length <= 2 || slideState === 1 ? (
+                <></>
+              ) : (
+                <button className="absolute top-1/2 right-2 translate-y-[-50%] z-50" onClick={handleNext}>
+                  <SlArrowRight className="text-4xl p-2 bg-white rounded-[50%] opacity-85" />
+                </button>
+              )}
+            </div>
+          </div>
+        </div>
+
+        <hr />
+
+        <div className="flex justify-between">
+          <div className="flex flex-col items-center justify-center">
+            <label htmlFor="image-input">
+              <div className="w-32 px-5 py-1 flex justify-around items-center cursor-pointer rounded-3xl border border-solid border-zinc-300 font-bold text-zinc-800">
+                <img src="/assets/addImageIcon.png" className="w-5" alt="#"></img>
+                <p className="text-xs">사진 첨부</p>
+              </div>
+            </label>
+            <input className="hidden" id="image-input" type="file" accept="image/*" onChange={(e) => handleImageChange(e)} multiple />
+          </div>
+
+          <button className="px-7 py-1 shadow-md rounded-3xl font-bold bg-amber-300 text-white">완료</button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default OkStartModal;
