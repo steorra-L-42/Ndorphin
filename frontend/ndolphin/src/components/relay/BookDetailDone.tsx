aa2777c470d03931bc3b600ebdfd3d3cefbab818
박예진 <eksk9044@naver.com>
2024-07-30 23:37:57 +0900

    [FE] Design: 릴레이북 수정 및 종료, 이미 작성 페이지 디자인 - 릴레이북 수정 form - 릴레이북 페이지 수정 form - 이미 페이지를 추가한 이야기일 경우 추가 불가능 문구 디자인 - 종료된 이야기일 경우의 페이지 디자인 중

---
+++ frontend/ndolphin/src/components/relay/BookDetailDone.tsx
@@ -0,0 +1,9 @@
+const BookDetailDone = () => {
+  return (
+    <div className="h-full flex flex-col justify-center">
+      <p className="text-outline text-2xl font-bold drop-shadow-md text-zinc-600">이야기가 끝났습니다</p>
+    </div>
+  );
+}
+
+export default BookDetailDone;
