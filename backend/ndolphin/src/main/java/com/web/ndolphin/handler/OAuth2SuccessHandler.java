ea47e16f78ba7950cf3f0da86bc46447b91b3c7a
이성현 <vkse1934@naver.com>
2024-07-31 14:04:08 +0900

    [BE] Chore: baseUrl 변경 localhost -> ec2

--- backend/ndolphin/src/main/java/com/web/ndolphin/handler/OAuth2SuccessHandler.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/handler/OAuth2SuccessHandler.java
@@ -1,17 +1,15 @@
 package com.web.ndolphin.handler;
 
 import com.web.ndolphin.domain.CustomOAuth2User;
-import com.web.ndolphin.provider.JwtProvider;
 import com.web.ndolphin.util.LogUtil;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.web.authentication.SimpleUrlAuthenticationSuccessHandler;
 import org.springframework.stereotype.Component;
-
-import java.io.IOException;
 
 @Component
 @RequiredArgsConstructor
@@ -19,7 +17,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 
     // OAuth2UserServiceImpl에서 성공적으로 CustomOAuth2User를 반환하면 메서드 실행
     @Override
-    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
+        Authentication authentication) throws IOException, ServletException {
         LogUtil.info("authentication.getPrincipal()", authentication.getPrincipal());
 
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
@@ -32,7 +31,8 @@ public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler 
 //        response.sendRedirect("http://localhost:8000/api/v1/auth/test");
 
         // OAuth2 로그인 성공하면 컨트롤러로 redirect
-        String redirectUrl = "http://localhost:8080/api/v1/auth/oauth-response/" + userId;
+        String redirectUrl =
+            "http://ec2-54-180-146-64.ap-northeast-2.compute.amazonaws.com:8080/api/v1/auth/oauth-response/" + userId;
         getRedirectStrategy().sendRedirect(request, response, redirectUrl);
     }
 }
