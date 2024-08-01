d4197d96b9b85f280c7f686721d82698e5db9b8d
이성현 <vkse1934@naver.com>
2024-08-01 11:31:21 +0900

    [BE] Feat: 신규 유저 정보 확인 기능

--- backend/ndolphin/src/main/java/com/web/ndolphin/dto/auth/response/OAuth2ResponseDto.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/dto/auth/response/OAuth2ResponseDto.java
@@ -1,9 +1,7 @@
 package com.web.ndolphin.dto.auth.response;
 
-import com.web.ndolphin.domain.LoginType;
 import com.web.ndolphin.domain.Token;
 import com.web.ndolphin.dto.ResponseDto;
-import com.web.ndolphin.util.LogUtil;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
@@ -16,17 +14,22 @@ import org.springframework.http.ResponseEntity;
 public class OAuth2ResponseDto {
 
     private Long userId;
+    private boolean isNewUser;
     private String accessToken;
     private String refreshToken;
 
-    public OAuth2ResponseDto(Long userId, Token token) {
+    public OAuth2ResponseDto(Long userId, boolean isNewUser, Token token) {
         this.accessToken = token.getAccessToken();
         this.refreshToken = token.getRefreshToken();
+        this.isNewUser = isNewUser;
         this.userId = userId;
     }
 
     public static ResponseEntity<ResponseDto> success(Token token) {
-        OAuth2ResponseDto data = new OAuth2ResponseDto(token.getUser().getUserId(), token);
+
+        boolean isNewUser = token.getUser().getNickName() == null ? true : false;
+
+        OAuth2ResponseDto data = new OAuth2ResponseDto(token.getUser().getUserId(), isNewUser, token);
 
         ResponseDto<OAuth2ResponseDto> responseBody = new ResponseDto<>("SU", "Success", data);
 
