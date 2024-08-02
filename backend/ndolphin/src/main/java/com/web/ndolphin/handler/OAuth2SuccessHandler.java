d1f3e8418eb92c2626e9532655da4c13b4d877be
이성현 <vkse1934@naver.com>
2024-08-02 14:57:39 +0900

    [BE] Refactor: 토큰 발급 핸들러 처리

--- backend/ndolphin/src/main/java/com/web/ndolphin/handler/OAuth2SuccessHandler.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/handler/OAuth2SuccessHandler.java
@@ -1,8 +1,13 @@
 package com.web.ndolphin.handler;
 
 import com.web.ndolphin.domain.CustomOAuth2User;
+import com.web.ndolphin.domain.Token;
+import com.web.ndolphin.domain.User;
+import com.web.ndolphin.repository.TokenRepository;
+import com.web.ndolphin.repository.UserRepository;
 import com.web.ndolphin.util.LogUtil;
 import jakarta.servlet.ServletException;
+import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
@@ -15,25 +20,59 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class OAuth2SuccessHandler extends SimpleUrlAuthenticationSuccessHandler {
 
+    private final UserRepository userRepository;
+    private final TokenRepository tokenRepository;
+
     // OAuth2UserServiceImpl에서 성공적으로 CustomOAuth2User를 반환하면 메서드 실행
     @Override
     public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response,
         Authentication authentication) throws IOException, ServletException {
-        LogUtil.info("authentication.getPrincipal()", authentication.getPrincipal());
-
         CustomOAuth2User oAuth2User = (CustomOAuth2User) authentication.getPrincipal();
-
-        LogUtil.info("oAuth2User", oAuth2User);
 
         String userId = oAuth2User.getName();
 
-//        response.sendRedirect("http://localhost:3000/auth/oauth-response/" + token + "/3600");
-//        response.sendRedirect("http://localhost:8000/api/v1/auth/test");
+        LogUtil.info("onAuthenticationSuccess");
 
-        // OAuth2 로그인 성공하면 컨트롤러로 redirect
-        String redirectUrl =
-            "http://ec2-54-180-146-64.ap-northeast-2.compute.amazonaws.com:8080/api/v1/auth/oauth-response/" + userId;
+        User user = null;
 
-        getRedirectStrategy().sendRedirect(request, response, redirectUrl);
+        try {
+            user = userRepository.findByUserId(Long.valueOf(userId));
+
+            Token token = tokenRepository.findByUserId(Long.valueOf(userId));
+
+            // userId 쿠키 설정
+            Cookie userIdCookie = new Cookie("userId", userId);
+            userIdCookie.setMaxAge(3600); // 1시간 유효
+            userIdCookie.setPath("/"); // 모든 경로에서 접근 가능
+
+            // accessToken 쿠키 설정
+            Cookie jwtCookie = new Cookie("accessToken", token.getAccessToken());
+            jwtCookie.setMaxAge(3600); // 1시간 유효
+            jwtCookie.setPath("/"); // 모든 경로에서 접근 가능
+
+            // refreshToken 쿠키 설정
+            Cookie refreshTokenCookie = new Cookie("refreshToken", token.getRefreshToken());
+            refreshTokenCookie.setMaxAge(3600); // 1시간 유효
+            refreshTokenCookie.setPath("/"); // 모든 경로에서 접근 가능
+
+            String isNewUser = user.getNickName() == null ? "true" : "false";
+
+            Cookie isNewUserCookie = new Cookie("isNewUser", isNewUser);
+
+            isNewUserCookie.setMaxAge(3600);
+            isNewUserCookie.setPath("/");
+
+            // 쿠키를 응답에 추가
+            response.addCookie(userIdCookie);
+            response.addCookie(jwtCookie);
+            response.addCookie(refreshTokenCookie);
+
+            // 리다이렉트
+            response.sendRedirect("http://localhost:3000");
+//            response.sendRedirect("http://ec2-54-180-146-64.ap-northeast-2.compute.amazonaws.com:80
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
