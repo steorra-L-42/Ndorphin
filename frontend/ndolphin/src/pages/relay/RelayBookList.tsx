7cea832a667b2a2875421ee36931421b1a6a7c0b
ã박예진 <eksk9044@naver.com>
2024-08-04 16:53:21 +0900

    [FE] Design: 릴레이북 목록 페이지 상단 및 검색바 디자인 - 검색바 필터에 전체 추가 - 릴레이북 상단 디자인 변경 - 검색바에 검색 아이콘 추가 및 디자인 변경

--- frontend/ndolphin/src/pages/relay/RelayBookList.tsx
+++ frontend/ndolphin/src/pages/relay/RelayBookList.tsx
@@ -6,41 +6,50 @@ import SearchBar from "../../components/relay/SearchBar";
 function Relaybooklist() {
   const navigate = useNavigate();
   const [tabs, setTabs] = useState<number>(0);
-  const underline = "underline underline-offset-8 decoration-4 decoration-yellow-300";
+  const underline = "underline underline-offset-[10px] decoration-4 decoration-yellow-300";
 
   return (
     <div>
-      <div className="bg-yellow-100 flex items-center flex-col">
-        <div className="w-full h-60 py-10 flex flex-col justify-around">
-          <div>
-            <p className="text-center text-3xl font-bold">릴레이북</p>
-            <p className="text-center">'만약에~'를 이어 하나의 이야기를 완성시키는 릴레이북 게시판</p>
+      <div className="relative flex flex-col justify-center">
+        <div className="w-full px-44 flex-col items-center">
+          <div className="py-5 flex items-end">
+            <p className="text-xl font-bold">릴레이북</p>
+            <p className="pl-3 text-xs">‘만약에~’를 이어 하나의 이야기를 완성시키는 릴레이북 게시판</p>
           </div>
-          <SearchBar />
+          <div className="flex justify-center">
+            <button
+              className={`px-10 py-3 pb-5 font-semibold ${tabs === 0 ? underline : "text-[#6C6C6C]"} z-20`}
+              onClick={() => {
+                setTabs(0);
+              }}
+            >
+              진행 중
+            </button>
+            <button
+              className={`px-10 py-3 pb-5 font-semibold ${tabs === 1 ? underline : "text-[#6C6C6C]"} z-20`}
+              onClick={() => {
+                setTabs(1);
+              }}
+            >
+              완료
+            </button>
+          </div>
+          <hr className="w-full" />
+        </div>
+        <div className="flex items-center flex-col">
+          <div className="w-full py-6 pb-10 flex flex-col justify-around">
+            <SearchBar />
+          </div>
         </div>
       </div>
-      <div className="px-44 flex justify-center">
-        <button
-          className={`px-10 py-5 font-semibold ${tabs === 0 ? underline : "text-[#6C6C6C]"}`}
-          onClick={() => {
-            setTabs(0);
-          }}>
-          진행 중
-        </button>
-        <button
-          className={`px-10 py-5 font-semibold ${tabs === 1 ? underline : "text-[#6C6C6C]"}`}
-          onClick={() => {
-            setTabs(1);
-          }}>
-          완료
-        </button>
-      </div>
+
       <div className="px-44 text-right">
         <button
           className="px-7 py-1 shadow-md rounded-xl font-bold bg-amber-300 text-white"
           onClick={() => {
             navigate("/relaybookstart");
-          }}>
+          }}
+        >
           이야기 시작하기
         </button>
       </div>
