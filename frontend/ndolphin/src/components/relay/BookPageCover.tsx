c340438e7fd6e9b40f8400e6d9b68a35140e1599
ã박예진 <eksk9044@naver.com>
2024-08-07 21:39:19 +0900

    [FE] feat: 릴레이북 상세 표지 통신

--- frontend/ndolphin/src/components/relay/BookPageCover.tsx
+++ frontend/ndolphin/src/components/relay/BookPageCover.tsx
@@ -1,9 +1,25 @@
-import React, { useRef, useState, useCallback, ForwardedRef } from "react";
+import React, { useRef, useState, useCallback, ForwardedRef, useEffect } from "react";
 import DropDown from "./relayBookCRUD/RelayBookDropDown";
 import DeleteModal from "./relayBookCRUD/BookDeleteModal";
 
 interface BookPageCoverProps {
-  firstPage: any;
+  firstPage: {
+    id: number;
+    user: {};
+    subject: string;
+    content: string;
+    hit: number;
+    boardType: string;
+    createdAt: string;
+    updatedAt: string;
+    contentFileUrl: string;
+    hasParticipated: boolean;
+    maxPage: number;
+    commentResponseDtos: [];
+    reactionTypeCounts: {};
+    userReactionId: null;
+    userReactionType: string;
+  }[];
   bookId: any;
   isDeleteOpen: boolean;
   isAiOpen: boolean;
@@ -13,20 +29,32 @@ interface BookPageCoverProps {
 }
 
 const BookPageCover = React.forwardRef<HTMLDivElement, BookPageCoverProps>(({ firstPage, bookId, handleDelete }, ref: ForwardedRef<HTMLDivElement>) => {
+  const [contentFileUrl, setContentFileUrl] = useState("");
+  const [subject, setSubject] = useState("");
+    
+  useEffect(() => {
+    if (firstPage && firstPage.length > 0) {
+      setContentFileUrl(firstPage[0].contentFileUrl);
+      setSubject(firstPage[0].subject);
+    }
+  }, [firstPage]);
+
   return (
     <>
-      <div className="cover" ref={ref} data-density="hard">
-        <div className="h-full flex flex-col justify-between">
-          <div className="pt-5 pr-5 ">
-            <DropDown bookId={bookId} handleDelete={handleDelete} firstPage={firstPage} />
+      {
+        <div className="cover" ref={ref} data-density="hard">
+          <div className="h-full flex flex-col justify-between">
+            <div className="pt-5 pr-5 ">
+              <DropDown bookId={bookId} handleDelete={handleDelete} firstPage={firstPage} />
+            </div>
+            <div className="flex justify-center">
+              <img src={contentFileUrl} width="300px" alt="#"></img>
+            </div>
+            <p className="text-xl font-bold">{subject}</p>
+            <div className="w-full flex justify-end"></div>
           </div>
-          <div className="flex justify-center">
-            <img src="/assets/relay/relayStartSample1.png" width="300px" alt="#"></img>
-          </div>
-          <p className="text-xl font-bold">책 제목</p>
-          <div className="w-full flex justify-end"></div>
         </div>
-      </div>
+      }
     </>
   );
 });
