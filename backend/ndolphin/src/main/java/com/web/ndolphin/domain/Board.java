ceef0141d44083d082256648e396ee4ce1d8eca1
cvn1004 <cvn1004@naver.com>
2024-08-07 09:16:18 +0900

    [BE] Refactor: 게시글 생성시 상세보기
    - 게시글 상세보기 시 조회수 증가 문제 해결
    - ChatGPT 설정

--- backend/ndolphin/src/main/java/com/web/ndolphin/domain/Board.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/domain/Board.java
@@ -68,9 +68,4 @@ public class Board {
         hit = 0;
         createdAt = LocalDateTime.now();
     }
-
-    @PreUpdate
-    protected void onUpdate() {
-        updatedAt = LocalDateTime.now();
-    }
 }
