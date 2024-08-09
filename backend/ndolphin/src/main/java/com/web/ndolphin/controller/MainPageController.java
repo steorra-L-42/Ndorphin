da2baeaa4a42088d40576bfd97f79566f2d7622d
cvn1004 <cvn1004@naver.com>
2024-08-09 11:11:45 +0900

    [BE] Refactor: 메인페이지 API 분리

--- backend/ndolphin/src/main/java/com/web/ndolphin/controller/MainPageController.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/controller/MainPageController.java
@@ -33,7 +33,6 @@ public class MainPageController {
     private final UserService userService;
     private final BoardService boardService;
 
-    // 메인 페이지 조회 API
     @Operation(summary = "메인 페이지 데이터 조회", description = "메인 페이지에 필요한 데이터를 조회합니다.")
     @ApiResponses(value = {
         @ApiResponse(responseCode = "200", description = "메인 페이지 데이터 조회 성공",
@@ -42,26 +41,101 @@ public class MainPageController {
             content = @Content(schema = @Schema()))
     })
     @GetMapping("/main")
-    public ResponseEntity<ResponseDto> getMainPageData(
-        @Parameter(description = "릴레이 게시판 기간 (daily, weekly, monthly)", example = "daily", required = false)
-        @RequestParam(value = "relayPeriod", defaultValue = "daily") String relayPeriod,
+    public ResponseEntity<ResponseDto> getMainPageData() {
 
-        @Parameter(description = "벨런스 게시판 기간 (daily, weekly, monthly)", example = "daily", required = false)
-        @RequestParam(value = "balancePeriod", defaultValue = "daily") String balancePeriod,
+        String fixedPeriod = "daily";
 
-        @Parameter(description = "if 게시판 기간 (daily, weekly, monthly)", example = "daily", required = false)
-        @RequestParam(value = "ifPeriod", defaultValue = "daily") String ifPeriod,
-
-        @Parameter(description = "더보기 플래그 값 (true, false)", example = "false", required = false)
-        @RequestParam(value = "flag", defaultValue = "false") boolean flag) {
-
-        List<RelayBoardDetailResponseDto> relayBoards = boardService.getRelayBoards(relayPeriod);
-        List<VoteBoardDetailResponseDto> voteBoards = boardService.getVoteBoards(balancePeriod);
-        List<OpinionBoardDetailResponseDto> opinionBoards = boardService.getOpinionBoards(ifPeriod);
-        List<BestNResponseDto> bestNs = userService.getSortedUsersByNPoint(flag);
+        List<RelayBoardDetailResponseDto> relayBoards = boardService.getRelayBoards(fixedPeriod);
+        List<VoteBoardDetailResponseDto> voteBoards = boardService.getVoteBoards(fixedPeriod);
+        List<OpinionBoardDetailResponseDto> opinionBoards = boardService.getOpinionBoards(fixedPeriod);
+        List<BestNResponseDto> bestNs = userService.getSortedUsersByNPoint(false);
 
         MainPageResponseDto mainPageResponse = new MainPageResponseDto(relayBoards, voteBoards, opinionBoards, bestNs);
         ResponseDto<?> responseBody = new ResponseDto<>(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, mainPageResponse);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+    
+    @Operation(summary = "릴레이 데이터 조회", description = "지정된 기간 동안의 릴레이 게시판 데이터를 조회합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "릴레이 데이터 조회 성공",
+            content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청",
+            content = @Content(schema = @Schema())),
+        @ApiResponse(responseCode = "500", description = "서버 오류",
+            content = @Content(schema = @Schema()))
+    })
+    @GetMapping("/main/relay")
+    public ResponseEntity<ResponseDto> getRelayData(
+        @Parameter(description = "릴레이 게시판 기간 (daily, weekly, monthly)", example = "daily", required = false)
+        @RequestParam(value = "relayPeriod", defaultValue = "daily") String relayPeriod) {
+
+        List<RelayBoardDetailResponseDto> relayBoards = boardService.getRelayBoards(relayPeriod);
+
+        ResponseDto<?> responseBody = new ResponseDto<>(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, relayBoards);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+    @Operation(summary = "밸런스 데이터 조회", description = "지정된 기간 동안의 밸런스 게시판 데이터를 조회합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "밸런스 데이터 조회 성공",
+            content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청",
+            content = @Content(schema = @Schema())),
+        @ApiResponse(responseCode = "500", description = "서버 오류",
+            content = @Content(schema = @Schema()))
+    })
+    @GetMapping("/main/balance")
+    public ResponseEntity<ResponseDto> getBalanceData(
+        @Parameter(description = "밸런스 게시판 기간 (daily, weekly, monthly)", example = "daily", required = false)
+        @RequestParam(value = "balancePeriod", defaultValue = "daily") String balancePeriod) {
+
+        List<VoteBoardDetailResponseDto> voteBoards = boardService.getVoteBoards(balancePeriod);
+
+        ResponseDto<?> responseBody = new ResponseDto<>(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, voteBoards);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+    @Operation(summary = "만약에 데이터 조회", description = "지정된 기간 동안의 '만약에' 게시판 데이터를 조회합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "만약에 데이터 조회 성공",
+            content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청",
+            content = @Content(schema = @Schema())),
+        @ApiResponse(responseCode = "500", description = "서버 오류",
+            content = @Content(schema = @Schema()))
+    })
+    @GetMapping("/main/if")
+    public ResponseEntity<ResponseDto> getIfData(
+        @Parameter(description = "만약에 게시판 기간 (daily, weekly, monthly)", example = "daily", required = false)
+        @RequestParam(value = "ifPeriod", defaultValue = "daily") String ifPeriod) {
+
+        List<OpinionBoardDetailResponseDto> opinionBoards = boardService.getOpinionBoards(ifPeriod);
+
+        ResponseDto<?> responseBody = new ResponseDto<>(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, opinionBoards);
+
+        return ResponseEntity.status(HttpStatus.OK).body(responseBody);
+    }
+
+    @Operation(summary = "베스트N 데이터 조회", description = "베스트N 사용자 목록을 조회합니다.")
+    @ApiResponses(value = {
+        @ApiResponse(responseCode = "200", description = "베스트N 데이터 조회 성공",
+            content = @Content(schema = @Schema(implementation = ResponseDto.class))),
+        @ApiResponse(responseCode = "400", description = "잘못된 요청",
+            content = @Content(schema = @Schema())),
+        @ApiResponse(responseCode = "500", description = "서버 오류",
+            content = @Content(schema = @Schema()))
+    })
+    @GetMapping("/main/bestN")
+    public ResponseEntity<ResponseDto> getBestNData(
+        @Parameter(description = "베스트N 더보기 플래그 값 (true, false)", example = "false", required = false)
+        @RequestParam(value = "flag", defaultValue = "false") boolean flag) {
+
+        List<BestNResponseDto> bestNs = userService.getSortedUsersByNPoint(flag);
+
+        ResponseDto<?> responseBody = new ResponseDto<>(ResponseCode.SUCCESS, ResponseMessage.SUCCESS, bestNs);
 
         return ResponseEntity.status(HttpStatus.OK).body(responseBody);
     }
