c36516d5e67aaaf6ed3078ef17e53392251c54dd
이철민 <lstork48@gmail.com>
2024-08-09 09:48:59 +0900

    [FE] Fix: 팔로워 안 나오는 문제 수정

--- frontend/ndolphin/src/components/user/FollowListItem.tsx
+++ frontend/ndolphin/src/components/user/FollowListItem.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 interface UserInfo {
   id: number;
@@ -14,8 +14,17 @@ interface FollowListItemProps {
 }
 
 const FollowListItem: React.FC<FollowListItemProps> = ({ follow, onFollowToggle, onClose }) => {
+  const [check, setCheck] = useState<number | null>(null);
+  
+  useEffect(() => {
+    const myUserId = Number(localStorage.getItem('userId'));
+    if (myUserId) {
+      setCheck(myUserId);
+    }
+  })
 
   const shiftProfile = () => {
+    console.log(follow)
     window.location.href = `/profile/${follow.id}`
     onClose();
   }
@@ -23,16 +32,23 @@ const FollowListItem: React.FC<FollowListItemProps> = ({ follow, onFollowToggle,
   const handleClick = async (event: React.MouseEvent<HTMLButtonElement>) => {
     event.stopPropagation();
     await onFollowToggle(follow.id);
-
   };
+
+  const renderButton = check !== follow.id;
 
   return (
     <div className="w-full px-8 py-2 flex items-center relative" onClick={shiftProfile}>
       <img className="w-10 h-10 rounded-full" src={follow.profileImage || "assets/user/profile.png"} alt="프로필 이미지" />
       <span className="ms-5 me-8 font-semibold">{follow.nickName}</span>
-      <button className={`ms-14 absolute right-16 text-xs w-auto h-auto p-2 border-none rounded-lg shadow-md transition duration-200 ease-in-out focus:outline-none focus:ring-2 ${follow.isFollowing ? "bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-300" : "bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-300"}`} onClick={handleClick}>
-        {follow.isFollowing ? "팔로잉" : "팔로우"}
-      </button>
+      {renderButton && (
+        <button
+          className={`ms-14 absolute right-16 text-xs w-auto h-auto p-2 border-none rounded-lg shadow-md transition duration-200 ease-in-out focus:outline-none focus:ring-2 ${
+            follow.isFollowing ? "bg-gray-500 text-white hover:bg-gray-600 focus:ring-gray-300" : "bg-blue-500 text-white hover:bg-blue-600 focus:ring-blue-300"
+          }`}
+          onClick={handleClick}>
+          {follow.isFollowing ? "팔로잉" : "팔로우"}
+        </button>
+      )}
     </div>
   );
 };
