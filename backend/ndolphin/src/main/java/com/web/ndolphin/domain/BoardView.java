f9f41ead57d05a4420da3697ff3fd1266b2481c1
차정혁 <ckwjdgur1234@gmail.com>
2024-08-09 15:35:12 +0900

    [BE] Feat: 유저가 해당 게시물을 조회 했었는지를 저장하는 BoardView 테이블 추가
     - BoardView테이블을 사용하여 만약에, 벨런스 개시물을 볼 떄 해당 유저가 보지 않은 3개의 게시물 목록을 전달하는데 사용함

--- backend/ndolphin/src/main/java/com/web/ndolphin/domain/BoardView.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/domain/BoardView.java
@@ -2,8 +2,6 @@ package com.web.ndolphin.domain;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,11 +14,11 @@ import lombok.Setter;
 @Entity
 @Getter
 @Setter
-public class Reaction extends DateEntity {
+public class BoardView extends DateEntity {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "reaction_id")
+    @Column(name = "board_view_id")
     private Long id;
 
     @ManyToOne(fetch = FetchType.LAZY)
@@ -30,7 +28,4 @@ public class Reaction extends DateEntity {
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "board_id")
     private Board board;
-
-    @Enumerated(EnumType.STRING)
-    private ReactionType reactionType;
 }
