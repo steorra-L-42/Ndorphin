b61d43c3661a7daf8cd76d51537ca551e089d87d
차정혁 <ckwjdgur1234@gmail.com>
2024-08-12 22:15:33 +0900

    [BE] Fix: 메인페이지 릴레이북 목록 조회 api 버그 수정
     - commentResponseDto가 null일때 처리를 안해줬음

--- backend/ndolphin/src/main/java/com/web/ndolphin/mapper/BoardMapper.java
+++ backend/ndolphin/src/main/java/com/web/ndolphin/mapper/BoardMapper.java
@@ -188,9 +188,12 @@ public class BoardMapper {
             relayBoardDetailResponseDto.setUserReactionType(ReactionType.NONE); // 기본 반응 타입
         }
 
-        if(board.getMaxPage() == commentResponseDtos.size() + 1){
+        if (commentResponseDtos != null
+            && board.getMaxPage() == commentResponseDtos.size() + 1) {
             relayBoardDetailResponseDto.setDone(true);
-        }else relayBoardDetailResponseDto.setDone(false);
+        } else {
+            relayBoardDetailResponseDto.setDone(false);
+        }
 
         return relayBoardDetailResponseDto;
     }
