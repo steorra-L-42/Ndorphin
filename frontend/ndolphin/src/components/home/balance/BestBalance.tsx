f27169f2f30ba054fd1de710a3dce68ac0fb033f
elumiryy <yerim5988jg@naver.com>
2024-08-12 21:41:43 +0900

    [FE] Design: 베스트 밸런스 타이틀 추가

--- frontend/ndolphin/src/components/home/balance/BestBalance.tsx
+++ frontend/ndolphin/src/components/home/balance/BestBalance.tsx
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import SwiperBalance from "./SwiperBalance";
+import RankingFilter from "../RankingFilter";
+import RankingDate from "../RankingDate";
 
 const BestBalance = () => {
+  const [rankingType, setRankingType] = useState("일간");
+
   return (
     <div className="">
+      <div className="flex items-end">
+        <span className="pr-5 text-2xl font-bold underline decoration-[#FFDE2F] decoration-4 underline-offset-8">Best 만약에</span>
+        <RankingFilter updateRankingType={setRankingType} />
+      </div>
+
+      <div className="py-4">
+        <RankingDate type={rankingType} />
+      </div>
+
       <SwiperBalance startIndex={0} delay={1500} />
       {/* <SwiperBalance startIndex={1} delay={1500} />
       <SwiperBalance startIndex={2} delay={1500} /> */}
