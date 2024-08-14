62e60be020ba4563d785c62f5277f9eca611ef9c
이철민 <lstork48@gmail.com>
2024-08-14 10:01:11 +0900

    [FE] Fix: 알림목록에서 닉네임이 두 번 표시되는 문제 수정

--- frontend/ndolphin/src/pages/ok/OkStartModal.tsx
+++ frontend/ndolphin/src/pages/ok/OkStartModal.tsx
@@ -132,9 +132,8 @@ const OkStartModal = ({ setIsCreateModal }: Props) => {
   // 괜찮아 등록 시 팔로워들에게 알림 전송
   const postAlarm = async () => {
     const userId = localStorage.getItem("userId");
-    const userNickName = localStorage.getItem('nickName')
     const response = await userApi.getFollower(userId as string);
-    const content = `${userNickName} 님이 새로운 괜찮아를 등록했습니다`;
+    const content = ' 님이 새로운 괜찮아를 등록했습니다';
     response.data.data.forEach((item: any) => {
       userApi.postNotifications(item.followerId, content, Number(userId));
     });
