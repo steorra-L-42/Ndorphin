a5a08562a303c3409ced2c360818e16abf08b35b
elumiryy <yerim5988jg@naver.com>
2024-08-15 20:25:44 +0900

    [FE] Feat: 베스트 릴레이 항목 클릭 시 상세 페이지로 연결

--- frontend/ndolphin/src/components/home/relay/ServeRelayBook.tsx
+++ frontend/ndolphin/src/components/home/relay/ServeRelayBook.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import { useNavigate } from "react-router";
 
 interface Props {
   currentIndex: number;
@@ -22,15 +23,23 @@ interface Relay {
 }
 
 const ServeRelayBook = ({ currentIndex, visibleBooks }: Props) => {
+  const navigate = useNavigate();
+
+  const goToDetail = (boardId: number) => {
+    navigate(`/relaybookdetail/${boardId}`);
+  };
+
   return (
     <div className="grid grid-cols-2 gap-x-5">
       {visibleBooks &&
         visibleBooks.map((book, index) => (
           <div key={book.id} className="flex flex-col justify-center">
-            <img className="w-full bg-white border aspect-1 object-cover" src={book.fileUrls ? book.fileUrls[0] : undefined} alt="" />
+            <img className="w-full bg-white border aspect-1 object-cover cursor-pointer" src={book.fileUrls ? book.fileUrls[0] : undefined} onClick={() => goToDetail(book.id)} alt="" />
             <div className="pt-1 flex font-semibold">
-              <p className="pr-1">{currentIndex + index + 1}</p>
-              <p className="line-clamp-1">{book.subject}</p>
+              <p className="pr-1 text-[#565656]">{currentIndex + index + 1}</p>
+              <p className="text-[#565656] line-clamp-1 hover:underline underline-[#565656] underline-offset-2 cursor-pointer" onClick={() => goToDetail(book.id)}>
+                {book.subject}
+              </p>
             </div>
           </div>
         ))}
