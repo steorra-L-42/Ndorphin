26141d52a57623354caaeb2c4a0da0cbeb15d71a
elumiryy <yerim5988jg@naver.com>
2024-08-15 22:06:20 +0900

    [FE] Design: 릴레이북 페이지 유저 정보의 프로필 이미지 비율 및 테두리 설정

--- frontend/ndolphin/src/components/relay/UserInfo.tsx
+++ frontend/ndolphin/src/components/relay/UserInfo.tsx
@@ -18,15 +18,20 @@ const UserInfo: React.FC<UserInfoProps> = ({ index, firstPage, user, userImage, 
   const localUserId = Number(localStorage.getItem("userId"));
 
   const handleUserClick = () => {
-    navigate(`/profile/${userId}`)
-  }
+    navigate(`/profile/${userId}`);
+  };
 
   return (
     <>
       <div className="px-14 py-2" key={user}>
         <div className="flex justify-between items-center">
           <div className="w-full flex items-center">
-            <img onClick={handleUserClick} className="w-9 h-9 mx-3 rounded-[50%] cursor-pointer hover:brightness-90 transition duration-200 ease-in-out" src={`${userImage ? userImage : "/assets/user/defaultProfile.png"}`} alt="" />
+            <img
+              onClick={handleUserClick}
+              className="w-9 h-9 mx-3 border rounded-[50%] cursor-pointer hover:brightness-90 transition duration-200 ease-in-out object-contain"
+              src={`${userImage ? userImage : "/assets/user/defaultProfile.png"}`}
+              alt=""
+            />
             <span className="w-full flex justify-start text-sm font-semibold">
               {user}
               {<img className="w-5 ml-1" src={`/assets/${badget === "N" ? "nBadget.png" : badget === "S" ? "sBadget.png" : "noBadget.png"}`} alt="badget" />}
